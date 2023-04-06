2e58be870dbf421367d86c00c9733deacd924187
Tom Runyon <tom@defenseunicorns.com>
2023-04-06 15:06:10 -0400

    unzip
    Signed-off-by: Tom Runyon <tom@defenseunicorns.com>

--- aws/extract.sh
+++ aws/extract.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set +o xtrace
 
 # Check if the runtime environment is Darwin (Mac OS X) or Linux
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -25,13 +26,10 @@ else
   ARCH_PROC=amd64
 fi
 
-ARCH_NAME=linux
-ARCH_PROC=amd64
-
 echo "HI!"
 echo "ARCH_NAME: ${ARCH_NAME}"
 echo "ARCH_PROC: ${ARCH_PROC}"
 
 unzip tmp/terraform_${1}_${ARCH_NAME}_${ARCH_PROC}.zip -d .
 mkdir -p run/loki/tf-plugins/registry.terraform.io/hashicorp/aws/${2}/${ARCH_NAME}_${ARCH_PROC}
-unzip -xf tmp/terraform-provider-aws_${2}_${ARCH_NAME}_${ARCH_PROC}.zip -d run/loki/tf-plugins/registry.terraform.io/hashicorp/aws/${2}/${ARCH_NAME}_${ARCH_PROC}
+unzip tmp/terraform-provider-aws_${2}_${ARCH_NAME}_${ARCH_PROC}.zip -d run/loki/tf-plugins/registry.terraform.io/hashicorp/aws/${2}/${ARCH_NAME}_${ARCH_PROC}
