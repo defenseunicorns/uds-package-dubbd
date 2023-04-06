fb2f84a8ab657b42af39c50fe84505db79901dbd
Tom Runyon <tom@defenseunicorns.com>
2023-04-06 08:03:39 -0400

    add linux versions for terraform
    Signed-off-by: Tom Runyon <tom@defenseunicorns.com>

---
+++ aws/extract.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+# Check if the runtime environment is Darwin (Mac OS X) or Linux
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  ARCH_NAME=darwin
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  ARCH_NAME=linux
+else
+  echo "The OS is not supported"
+  exit 1
+fi
+
+# Check the processor architecture
+if [[ $(uname -m) == "x86_64" ]]; then
+  echo "The processor architecture is 64-bit"
+  if [[ $(lscpu | grep 'Vendor ID' | awk '{print $3}') == "AuthenticAMD" ]]; then
+    ARCH_PROC=amd64
+  fi
+elif [[ $(uname -m) == "i686" || $(uname -m) == "i386" ]]; then
+  echo "The processor architecture is 32-bit"
+  echo "The processor is not AMD or ARM"
+elif [[ $(uname -m) == "arm64" ]]; then
+  ARCH_PROC=arm64
+else
+  echo "The processor architecture is not supported"
+  exit 1
+fi
+
+echo "HI!"
+echo "ARCH_NAME: ${ARCH_NAME}"
+echo "ARCH_PROC: ${ARCH_PROC}"
+
+tar -xf tmp/terraform_${1}_${ARCH_NAME}_${ARCH_PROC}.zip -C .
+mkdir -p run/loki/tf-plugins/registry.terraform.io/hashicorp/aws/${2}/${ARCH_NAME}_${ARCH_PROC}
+tar -xf tmp/terraform-provider-aws_${2}_${ARCH_NAME}_${ARCH_PROC}.zip -C run/loki/tf-plugins/registry.terraform.io/hashicorp/aws/${2}/${ARCH_NAME}_${ARCH_PROC}
