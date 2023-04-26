de32aa3165100bb8e11f7167798e3582eae07ff8
Kiran Thyagaraja <18704819+kirankt@users.noreply.github.com>
2023-04-25 19:32:08 -0500

    Fully working example of cocowow+podinfo

--- cocowow/scripts/update-authservice-secret.sh
+++ cocowow/scripts/update-authservice-secret.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 # encode the updated json back to base64
-ENCODED_VALUE=$(kubectl get secret authservice -n authservice -o jsonpath='{.data.config\.json}'|base64 -d|sed "s/CHANGE_ME_MISSION_APP_SECRET/###ZARF_VAR_MISSION_APP_SECRET###/g" | sed "s/CHANGE_ME_MISSION_APP_ID/###ZARF_VAR_MISSION_APP_ID###/g"|base64|tr -d '\n' | sed 's/ //g')
+ENCODED_VALUE=$(kubectl get secret authservice -n authservice -o jsonpath='{.data.config\.json}'|base64 -d|sed "s/CHANGE_ME_MISSION_APP_SECRET/###ZARF_VAR_MISSION_APP_SECRET###/g"|base64|tr -d '\n' | sed 's/ //g')
 
 echo $ENCODED_VALUE|base64 -d
 
