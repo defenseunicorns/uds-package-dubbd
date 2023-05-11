8722fb01d8b3e3e456b69d3120221000a9aaa353
Justin Oursler <justin.oursler@defenseunicorns.com>
2023-05-11 14:42:26 -0500

    Initial commit of using UDS IaC S3 and KMS modules.

--- aws/loki/main.tf
+++ aws/loki/main.tf
@@ -3,17 +3,41 @@ provider "aws" {
 
 }
 
+# taken from zarf bb repo
+resource "random_id" "default" {
+  byte_length = 2
+}
 
+# taken from zarf bb repo
+locals{
+    kms_key_alias_name_prefix  = "alias/dubbd-aws-testing-${lower(random_id.default.hex)}"
+}
+
+# taken from zarf bb repo
+variable "kms_key_deletion_window" {
+  description = "Waiting period for scheduled KMS Key deletion. Can be 7-30 days."
+  type        = number
+  default     = 7
+}
+
+module "kms_key" {
+  source = "github.com/defenseunicorns/uds-iac-aws-kms?ref=oursler-initial-testing"
+  kms_key_description = "KMS Key for DUBBD"
+  kms_key_deletion_window = var.kms_key_deletion_window
+  kms_key_alias_name_prefix = local.kms_key_alias_name_prefix
+}
 
 module "S3" {
-    source = "github.com/defenseunicorns/delivery-aws-iac//modules/s3-irsa"
-    name_prefix = "${var.name}"
-    eks_oidc_provider_arn = "${var.eks_oidc_provider_arn}"
-    kubernetes_service_account = "logging-loki"
-    kubernetes_namespace = "logging"
-    dynamodb_enabled = "false"
-    kms_key_arn = "${var.kms_key_arn}"
-    force_destroy = "${var.force_destroy}"
+  #source = "github.com/defenseunicorns/delivery-aws-iac//modules/s3-irsa"
+  source = "github.com/defenseunicorns/uds-iac-aws-s3?ref=oursler-initial-testing"
+  name_prefix = "${var.name}"
+  eks_oidc_provider_arn = "${var.eks_oidc_provider_arn}"
+  kubernetes_service_account = "logging-loki"
+  kubernetes_namespace = "logging"
+  #dynamodb_enabled = "false"
+  #kms_key_arn = "${var.kms_key_arn}"
+  kms_key_arn = "${module.kms_key.kms_key_arn}"
+  force_destroy = "${var.force_destroy}"
 }
 
 data "aws_region" "current" {}
@@ -34,9 +58,9 @@ output "s3_bucket" {
   value =  module.S3.s3_bucket
 }
 
-output "dynamodb_name" {
-  value =  module.S3.dynamodb_name
-}
+#output "dynamodb_name" {
+  #value =  module.S3.dynamodb_name
+#}
 
 variable "name" {
     description = "Name for cluster"
