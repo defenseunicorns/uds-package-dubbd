2bcbc59f8705f2a30a024f846166ca2330be3f52
Rob Ferguson <rob.ferguson@defenseunicorns.com>
2023-05-25 13:15:39 -0500

    disable unrelated

--- pepr-demo/deploy.sh
+++ pepr-demo/deploy.sh
@@ -2,6 +2,6 @@
 k3d cluster delete -c k3d.yaml
 k3d cluster create -c k3d.yaml
 
-zarf package create --architecture amd64 --confirm
-zarf init --components=git-server --confirm 
-zarf package deploy --confirm zarf-package-pepr-demo-amd64-2.1.0.tar.zst
+/home/rob/du/zarf/build/zarf package create --architecture amd64 --confirm
+/home/rob/du/zarf/build/zarf init --components=git-server --confirm
+/home/rob/du/zarf/build/zarf package deploy --tmpdir=/home/rob/rob-zarf --confirm /home/rob/du/zarf-package-big-bang/pepr-demo/zarf-package-pepr-demo-amd64-2.1.0.tar.zst
