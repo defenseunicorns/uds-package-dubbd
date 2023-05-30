911293a7b875f78f96d1dfe9568f6fe56fb10c15
Micah Nagel <micah.nagel@defenseunicorns.com>
2023-05-30 11:01:36 -0600

    zarf tools

--- defense-unicorns-distro/scripts/on_failure.sh
+++ defense-unicorns-distro/scripts/on_failure.sh
@@ -6,9 +6,9 @@ else
 fi
 
 # This will grab any helmreleases with a failed condition
-state=$(kubectl get helmrelease -n bigbang -o go-template='{{range $items,$contents := .items}}{{printf "HR %s" $contents.metadata.name}}{{printf " status is %s\n" (index $contents.status.conditions 0).reason}}{{end}}')
+state=$(zarf tools kubectl get helmrelease -n bigbang -o go-template='{{range $items,$contents := .items}}{{printf "HR %s" $contents.metadata.name}}{{printf " status is %s\n" (index $contents.status.conditions 0).reason}}{{end}}')
 failed=$(echo "${state}" | grep "Failed")
 failed_hrs=$(echo "{$failed}" | awk  '{print $2}')
 for hr in $failed_hrs; do
-    kubectl describe helmrelease -n bigbang $hr
+    zarf tools kubectl describe helmrelease -n bigbang $hr
 done
