c0a58299be3205ecabe819b651b3aa229e3adf7c
Micah Nagel <micah.nagel@defenseunicorns.com>
2023-06-05 09:44:11 -0600

    handle trigger reconcile behavior better

--- defense-unicorns-distro/scripts/reconcile_helmreleases.sh
+++
@@ -1,3 +0,0 @@
-for package in kyverno kyverno-policies istio-operator istio monitoring loki metrics-server tempo promtail neuvector kiali kyverno-reporter; do
-    zarf tools kubectl patch helmrelease -n bigbang $package --type=merge -p '{"metadata":{"annotations":{"reconcile.fluxcd.io/requestedAt":"'`date '+%F_%H:%M:%S.%s%z'`'"}}}' || true
-done
