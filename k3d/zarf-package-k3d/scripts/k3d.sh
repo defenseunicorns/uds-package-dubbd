5f072979d3a0afa0906c26fc2559299cc563f0ba
Tim Black <timblaktu@gmail.com>
2023-06-13 14:00:05 -0700

    major push forward in refactoring k3d-local sub-package into dubbd zarf package. Still untested, currently grappling with create-time parameter expansion.

--- k3d/zarf-package-k3d/scripts/k3d.sh
+++ k3d/zarf-package-k3d/scripts/k3d.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-
-CLUSTER_NAME=dubbd
+# Create k3d cluster named ${ZARF_K3D_CLUSTER_NAME}
+#
+# if it doesn't already exist
 
 # Check if the cluster already exists
-if k3d cluster list | grep -q "$CLUSTER_NAME"; then
+if k3d cluster list | grep -q "${ZARF_K3D_CLUSTER_NAME}"; then
   echo "Cluster already exists"
 else
-    # Local volume for data is tied to my implementation
     k3d cluster create \
     --k3s-arg "--disable=traefik@server:0" \
     --k3s-arg "--disable=metrics-server@server:0" \
     --port 443:443@loadbalancer \
-    --port 80:8080@loadbalancer \
-    --image ghcr.io/runyontr/packages/rancher/k3s:v1.25.7-k3s1-cuda \
-    --gpus=2 \
-    dubbd
+    --port 80:8080@loadbalancer
+    ${ZARF_K3D_CLUSTER_NAME}
 fi
-
 # Switch to the k3d context
-k3d kubeconfig merge $CLUSTER_NAME
+k3d kubeconfig merge ${ZARF_K3D_CLUSTER_NAME}
