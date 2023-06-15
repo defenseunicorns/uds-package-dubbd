fcbe5e8f83864304997d6ee8c416ebf6c89a0ea7
Tim Black <timblaktu@gmail.com>
2023-06-14 21:16:25 -0700

    fixed k3d cluster creation and fetching/deploying init pkg. Package remove successfully deletes the cluster, but zarf barfs at the very end - something about not being able to access a secret.

--- k3d/zarf-package-k3d/README.md
+++ k3d/zarf-package-k3d/README.md
@@ -4,15 +4,17 @@ Zarf package that deploys a k3d cluster
 
 ## Creation
 
-Confirm your desired values are in [`zarf-config.yaml`](./zarf-config.yaml), then run:
+Confirm your desired values are in [`zarf-config.yaml`](./zarf-config.yaml)[^1], then run:
 
 
 ```bash
-zarf package create
+zarf package create --confirm
 ```
 
 ## Deployment
 
 ```bash
-zarf package deploy
+zarf package deploy --confirm
 ```
+
+[^1]: Noting that only global `zarf` options may be declared, and that `--confirm` is not a global option.
--- k3d/zarf-package-k3d/scripts/k3d.sh
+++ k3d/zarf-package-k3d/scripts/k3d.sh
@@ -3,16 +3,19 @@
 #
 # if it doesn't already exist
 
-# Check if the cluster already exists
-if k3d cluster list | grep -q "${ZARF_K3D_CLUSTER_NAME}"; then
+[[ "$#" -ne 1 ]] && echo "ERROR: must specify K3D_CLUSTER_NAME in single arg" && exit 1
+K3D_CLUSTER_NAME=$1
+if k3d cluster list | grep -q "${K3D_CLUSTER_NAME}"; then
   echo "Cluster already exists"
 else
     k3d cluster create \
-    --k3s-arg "--disable=traefik@server:0" \
-    --k3s-arg "--disable=metrics-server@server:0" \
+    --k3s-arg "--disable=traefik@server:*" \
+    --k3s-arg "--disable=metrics-server@server:*" \
+    --k3s-arg "--disable=servicelb@server:*" \
     --port 443:443@loadbalancer \
-    --port 80:8080@loadbalancer
-    ${ZARF_K3D_CLUSTER_NAME}
+    --port 80:8080@loadbalancer \
+    ${K3D_CLUSTER_NAME}
 fi
-# Switch to the k3d context
-k3d kubeconfig merge ${ZARF_K3D_CLUSTER_NAME}
+# Write or merge kubeconfig(s) from k3d cluster(s) into kubeconfig file,
+# and switch kubectl to this context
+k3d kubeconfig merge -s ${K3D_CLUSTER_NAME}
