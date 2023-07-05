bc22d5a70c2247df2296cce7b91cf8c948267ea2
MxNxPx <MxNxPx@gmail.com>
2023-07-05 11:28:55 -0400

    feat: updating external-dns TF & dubbd-aws [ci skip]
    Co-authored-by: Will C <wirewc@users.noreply.github.com>
    Co-authored-by: Noah <noahpb@users.noreply.github.com>
    Co-authored-by: Micah Nagel <mjnagel@users.noreply.github.com>

--- aws/route53-policy/main.tf
+++ aws/route53-policy/main.tf
@@ -6,7 +6,21 @@ terraform {
   }
 }
 
-resource "aws_iam_policy" "route53_role" {
+data "aws_eks_cluster" "existing" {
+  name = var.name
+}
+
+module "iam_assumable_role_admin" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "5.27.0"
+  create_role                   = true
+  role_name                     = "external-dns-${var.name}"
+  provider_url                  = substr(data.aws_eks_cluster.existing.identity[0].oidc[0].issuer, 8, -1)
+  role_policy_arns              = [length(aws_iam_policy.external_dns) >= 1 ? aws_iam_policy.external_dns.arn : ""]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:external-dns:external-dns"]
+}
+
+resource "aws_iam_policy" "external_dns" {
   name        = "AllowExternalDNSUpdates"
   path        = "/"
   description = "Policy for external-dns to create records in route53 hosted zones. Created by DUBBD."
@@ -14,31 +28,28 @@ resource "aws_iam_policy" "route53_role" {
   # Terraform expression result to valid JSON syntax.
   policy = jsonencode(
 {
-  "Version": "2012-10-17",
-  "Statement": [
+  Version = "2012-10-17",
+  Statement = [
     {
-      "Effect": "Allow",
-      "Action": [
-        "route53:ChangeResourceRecordSets"
-      ],
-      "Resource": [
-        "arn:aws:route53:::hostedzone/*"
+      Effect = "Allow",
+      Action = [
+        "route53ChangeResourceRecordSets"
       ]
-    },
+      Resource = [
+        "arnaws:route53:::hostedzone/*"
+      ]
+    }
     {
-      "Effect": "Allow",
-      "Action": [
-        "route53:ListHostedZones",
-        "route53:ListResourceRecordSets",
-        "route53:ListTagsForResource"
-      ],
-      "Resource": [
+      Effect = "Allow"
+      Action = [
+        "route53ListHostedZones",
+        "route53ListResourceRecordSets",
+        "route53ListTagsForResource",
+      ]
+      Resource = [
         "*"
       ]
     }
   ]
 })
 }
-
-#TODO: use the aws public modules to configure iam and irsa, see example below
-# https://github.com/defenseunicorns/terraform-aws-uds-s3/blob/1c098dc739e096c0425a23ba6bdf3c84feb37e18/main.tf#L106
---
+++ aws/route53-policy/terraform.tfvars
@@ -0,0 +1,2 @@
+name = "###ZARF_VAR_NAME###"
+eks_cluster_oidc_arn = "###ZARF_VAR_EKS_CLUSTER_OIDC_ARN###"
