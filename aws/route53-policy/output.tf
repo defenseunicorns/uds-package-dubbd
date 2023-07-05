76110d4fe33b771d8ab27a97af5dae713a7af487
MxNxPx <MxNxPx@gmail.com>
2023-07-05 15:43:04 -0400

    fix: TF output value [skip ci]
    Co-authored-by: Micah Nagel <mjnagel@users.noreply.github.com>
    Co-authored-by: Will C <wirewc@users.noreply.github.com>
    Co-authored-by: Noah <noahpb@users.noreply.github.com>

--- aws/route53-policy/output.tf
+++ aws/route53-policy/output.tf
@@ -1,4 +1,4 @@
 output "route53_role_arn" {
   description = "ARN of the IAM Policy created for external-dns to manage records in route53."
-  value = aws_iam_policy.external_dns.arn
+  value = module.iam_assumable_role_admin.iam_role_arn
 }
