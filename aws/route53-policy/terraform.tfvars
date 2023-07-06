3dca1790dbdae070a82eaf66e4833a0423e475ea
MxNxPx <MxNxPx@gmail.com>
2023-07-06 15:43:35 -0400

    fix: cleanup unused TF vars

--- aws/route53-policy/terraform.tfvars
+++ aws/route53-policy/terraform.tfvars
@@ -1,2 +1,1 @@
 name = "###ZARF_VAR_NAME###"
-eks_cluster_oidc_arn = "###ZARF_VAR_EKS_CLUSTER_OIDC_ARN###"
--- aws/route53-policy/variables.tf
+++ aws/route53-policy/variables.tf
@@ -1,6 +1,3 @@
 variable "name" {
   description = "Name for cluster"
 }
-variable "eks_cluster_oidc_arn" {
-  description = "The ARN of the OIDC Provider of the EKS Cluster"
-}
