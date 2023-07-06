48cdfd66841222e896998bbd1fbf1938d28c0d09
MxNxPx <MxNxPx@gmail.com>
2023-07-06 10:27:04 -0400

    fix: update policy & role names
    Co-authored-by: Will C <wirewc@users.noreply.github.com>
    Co-authored-by: Noah <noahpb@users.noreply.github.com>

--- aws/route53-policy/main.tf
+++ aws/route53-policy/main.tf
@@ -14,14 +14,14 @@ module "iam_assumable_role_admin" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version                       = "5.27.0"
   create_role                   = true
-  role_name                     = "external-dns-${var.name}"
+  role_name                     = "external-dns-role-${var.name}"
   provider_url                  = substr(data.aws_eks_cluster.existing.identity[0].oidc[0].issuer, 8, -1)
   role_policy_arns              = [length(aws_iam_policy.external_dns) >= 1 ? aws_iam_policy.external_dns.arn : ""]
   oidc_fully_qualified_subjects = ["system:serviceaccount:external-dns:external-dns"]
 }
 
 resource "aws_iam_policy" "external_dns" {
-  name        = "AllowExternalDNSUpdates"
+  name        = "external-dns-policy-${var.name}"
   path        = "/"
   description = "Policy for external-dns to create records in route53 hosted zones. Created by DUBBD."
 
