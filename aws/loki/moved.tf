576dc76fbd499d3c56c00b08c9fb717453b20d95
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2023-07-12 12:45:25 -0500

    Run terraform fmt

--- aws/loki/moved.tf
+++ aws/loki/moved.tf
@@ -1,16 +1,16 @@
 moved {
-    from = module.S3.module.irsa_policy[0].aws_iam_policy.policy[0]
-    to = aws_iam_policy.loki_policy
+  from = module.S3.module.irsa_policy[0].aws_iam_policy.policy[0]
+  to   = aws_iam_policy.loki_policy
 }
 
 moved {
-    from = module.S3.aws_iam_role_policy_attachment.irsa[0]
-    to = module.irsa.module.irsa.aws_iam_role_policy_attachment.custom[0]
+  from = module.S3.aws_iam_role_policy_attachment.irsa[0]
+  to   = module.irsa.module.irsa.aws_iam_role_policy_attachment.custom[0]
 }
 
 moved {
-    from = module.S3.module.irsa[0].aws_iam_role.this[0]
-    to = module.irsa.module.irsa.aws_iam_role.this[0]
+  from = module.S3.module.irsa[0].aws_iam_role.this[0]
+  to   = module.irsa.module.irsa.aws_iam_role.this[0]
 }
 
 moved {
