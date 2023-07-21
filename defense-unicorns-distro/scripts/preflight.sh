f5c7a7350b5e54e158725547dda0667746e29c78
Micah Nagel <micah.nagel@defenseunicorns.com>
2023-07-21 15:21:22 -0600

    chore: update packaging standards, add yamllint in CI (#414)
    * chore: cleanup bad zarf practices
    
    * chore: cat+indent changes
    
    * ci: yamllint additions
    
    * chore: fixy-wixy [ci skip]
    
    * chore: fix yamllint
    
    * chore: fix zarf in k3d local [ci skip]
    
    * chore: lint all the things [ci skip]
    
    * ci: test with needs jobs
    
    * ci: workflow for cloudtrail [ci skip]
    
    * chore: fix all the lints
    
    * fix: move preflight further down for uds core
    
    * chore: desc [ci skip]
    
    * chore: ./zarf [ci skip]
    
    * chore: lint some more

--- defense-unicorns-distro/scripts/preflight.sh
+++ defense-unicorns-distro/scripts/preflight.sh
@@ -3,10 +3,8 @@
 # Run your command and get the output
 output="###ZARF_VAR_OUTPUT###"
 
-echo ${output}
 # Extract the Server Version Major and Minor versions using grep and cut
 server_version=$(echo "$output" | grep -o "Server Version:.*" | cut -d" " -f3)
-echo "server_version_line: ${server_version}"
 server_major_version=$(echo "$server_version" | cut -d"." -f1)
 server_minor_version=$(echo "$server_version" | cut -d"." -f2)
 
@@ -14,14 +12,12 @@ server_minor_version=$(echo "$server_version" | cut -d"." -f2)
 supported_major_version="v1"
 supported_minor_version="26"
 
-echo "Server Major: ${server_major_version}"
-echo "Server Minor: ${server_minor_version}"
-
 # Check if the Server Version is supported
 if [[ "$server_major_version" == "$supported_major_version" ]] && (( "$server_minor_version" >= "$supported_minor_version" )); then
   echo "Server version is supported"
   exit 0
 else
   echo "Server version is not supported"
+  echo "Please update your cluster to a minimum Kubernetes version of ${supported_major_version}.${server_minor_version}"
   exit 1
 fi
