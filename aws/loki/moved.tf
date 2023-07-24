ab9faf043791b1970e925cc45616d95c2f647716
Noah Birrer <noah@defenseunicorns.com>
2023-07-24 09:36:56 -0400

    add moved blocks for new module

---
+++ aws/loki/moved.tf
@@ -0,0 +1,14 @@
+moved {
+  to =  module.irsa.module.irsa.aws_iam_role.this[0]
+  from = module.S3.module.irsa[0].aws_iam_role.this[0]
+}
+
+moved {
+  to = aws_iam_policy.loki_policy
+  from = module.S3.module.irsa_policy[0].aws_iam_policy.policy[0]
+}
+
+moved {
+  to = module.irsa.module.irsa.aws_iam_role_policy_attachment.this["loki"]
+  from = module.S3.aws_iam_role_policy_attachment.irsa[0]
+}
