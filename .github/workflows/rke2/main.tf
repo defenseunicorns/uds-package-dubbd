fa89b2363bd224f50593cd5f06c6846fb88d8c1f
Micah Nagel <micah.nagel@defenseunicorns.com>
2023-08-18 11:01:01 -0600

    chore: try one large server

--- .github/workflows/rke2/main.tf
+++ .github/workflows/rke2/main.tf
@@ -8,32 +8,32 @@ terraform {
 }
 
 module "rke2" {
-  source  = "github.com/rancherfederal/rke2-aws-tf?ref=v2.4.0"
-  cluster_name    = var.name
-  vpc_id  = var.vpc_id
-  subnets = var.subnets
-  ami     = var.ami
-  servers       = 3
-  instance_type = "m5.2xlarge"
+  source        = "github.com/rancherfederal/rke2-aws-tf?ref=v2.4.0"
+  cluster_name  = var.name
+  vpc_id        = var.vpc_id
+  subnets       = var.subnets
+  ami           = var.ami
+  servers       = 1
+  instance_type = "m5.4xlarge"
   tags = {
     name = var.name
   }
   block_device_mappings = {
-    size = 100
+    size      = 100
     encrypted = true
-    type = "gp3"
+    type      = "gp3"
   }
-  enable_ccm = true
-  iam_permissions_boundary = var.iam_permissions_boundary
-  download = false
-  rke2_config = <<-EOF
+  enable_ccm                  = true
+  iam_permissions_boundary    = var.iam_permissions_boundary
+  download                    = false
+  rke2_config                 = <<-EOF
 disable:
   - rke2-ingress-nginx
   - rke2-metrics-server
 EOF
-  controlplane_internal = var.controlplane_internal
+  controlplane_internal       = var.controlplane_internal
   associate_public_ip_address = var.associate_public_ip_address
-  wait_for_capacity_timeout = "20m"
+  wait_for_capacity_timeout   = "20m"
 }
 
 resource "null_resource" "kubeconfig" {
--- .github/workflows/rke2/variables.tf
+++ .github/workflows/rke2/variables.tf
@@ -1,48 +1,48 @@
 variable "name" {
   description = "Name for cluster"
   type        = string
-  default = "dubbd-rke2-ci"
+  default     = "dubbd-rke2-ci"
 }
 
 variable "vpc_id" {
   type        = string
   description = "VPC ID to deploy into"
-  default = "vpc-0a069641d8ea3aba6"
+  default     = "vpc-0a069641d8ea3aba6"
 }
 
 variable "subnets" {
-  type = list(string)
+  type        = list(string)
   description = "List of subnets to deploy into"
-  default     = ["subnet-0d0da65a31bcaa9dc","subnet-0f9c052423f4ca602","subnet-0bf47e35d4b60f338"]
+  default     = ["subnet-0d0da65a31bcaa9dc", "subnet-0f9c052423f4ca602", "subnet-0bf47e35d4b60f338"]
 }
 
 variable "ami" {
-  type = string
+  type        = string
   description = "AMI to use for deployment, must have RKE2 pre-installed"
   # https://github.com/defenseunicorns/uds-rke2-image-builder
   default = "ami-0d5808d974253f6b8"
 }
 
 variable "region" {
-  type = string
+  type        = string
   description = "Region to use for deployment"
-  default = "us-west-2"
+  default     = "us-west-2"
 }
 
 variable "controlplane_internal" {
-  type = bool
+  type        = bool
   description = "Make controlplane internal"
-  default = false # Default public for CI
+  default     = false # Default public for CI
 }
 
 variable "associate_public_ip_address" {
-  type = bool
+  type        = bool
   description = "Add public IPs for nodes"
-  default = true # Default public for CI
+  default     = true # Default public for CI
 }
 
 variable "iam_permissions_boundary" {
-  type = string
+  type        = string
   description = "Permissions boundary for IAM Role"
-  default = "arn:aws:iam::248783118822:policy/unicorn-base-policy"
+  default     = "arn:aws:iam::248783118822:policy/unicorn-base-policy"
 }
