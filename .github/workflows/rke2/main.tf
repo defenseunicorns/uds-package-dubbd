6a48b77cbbdceb246445dfbbeb5a25f44c02ec92
Micah Nagel <micah.nagel@defenseunicorns.com>
2023-08-21 11:06:50 -0600

    chore: switch rke2 CI to uds-ami (#508)
    * chore: switch rke2 CI to uds-ami
    
    * fix: awscli fix, increased timeout
    
    * chore: try one large server
    
    * chore: 1server,2agent

--- .github/workflows/rke2/main.tf
+++ .github/workflows/rke2/main.tf
@@ -8,88 +8,63 @@ terraform {
 }
 
 module "rke2" {
-  source  = "github.com/rancherfederal/rke2-aws-tf?ref=v2.4.0"
-  cluster_name    = var.name
-  vpc_id  = var.vpc_id
-  subnets = var.subnets
-  ami     = var.ami
-  servers       = 3
+  source        = "github.com/rancherfederal/rke2-aws-tf?ref=v2.4.0"
+  cluster_name  = var.name
+  vpc_id        = var.vpc_id
+  subnets       = var.public_subnets
+  ami           = var.ami
+  servers       = 1
   instance_type = "m5.2xlarge"
   tags = {
     name = var.name
   }
   block_device_mappings = {
-    size = 100
+    size      = 100
     encrypted = true
-    type = "gp3"
+    type      = "gp3"
   }
-  enable_ccm = true
-  iam_permissions_boundary = var.iam_permissions_boundary
-
-  rke2_version = "v1.26.6+rke2r1"
-  rke2_config = <<-EOF
+  enable_ccm                  = true
+  iam_permissions_boundary    = var.iam_permissions_boundary
+  download                    = false
+  rke2_config                 = <<-EOF
 disable:
   - rke2-ingress-nginx
+  - rke2-metrics-server
 EOF
-  controlplane_internal = var.controlplane_internal
+  controlplane_internal       = var.controlplane_internal
   associate_public_ip_address = var.associate_public_ip_address
+  wait_for_capacity_timeout   = "20m"
+}
 
-  pre_userdata = <<EOF
-sudo sh -c '
-  iptables -A INPUT -p tcp -m tcp --dport 2379 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p tcp -m tcp --dport 2380 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p tcp -m tcp --dport 9345 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p tcp -m tcp --dport 6443 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p udp -m udp --dport 8472 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p tcp -m tcp --dport 10250 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p tcp -m tcp --dport 30000:32767 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p tcp -m tcp --dport 4240 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p tcp -m tcp --dport 179 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p udp -m udp --dport 4789 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p tcp -m tcp --dport 5473 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p tcp -m tcp --dport 9098 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p tcp -m tcp --dport 9099 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p udp -m udp --dport 51820 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p udp -m udp --dport 51821 -m state --state NEW -j ACCEPT
-  iptables -A INPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-  iptables -A OUTPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-  iptables-save
+module "agents" {
+  source = "github.com/rancherfederal/rke2-aws-tf//modules/agent-nodepool?ref=v2.4.0"
 
-  echo "* soft nofile 13181250" >> /etc/security/limits.d/ulimits.conf
-  echo "* hard nofile 13181250" >> /etc/security/limits.d/ulimits.conf
-  echo "* soft nproc  13181250" >> /etc/security/limits.d/ulimits.conf
-  echo "* hard nproc  13181250" >> /etc/security/limits.d/ulimits.conf
-
-  sysctl -w vm.max_map_count=524288
-  echo "vm.max_map_count=524288" > /etc/sysctl.d/vm-max_map_count.conf
-
-  sysctl -w fs.nr_open=13181252
-  echo "fs.nr_open=13181252" > /etc/sysctl.d/fs-nr_open.conf
-
-  sysctl -w fs.file-max=13181250
-  echo "fs.file-max=13181250" > /etc/sysctl.d/fs-file-max.conf
-
-  echo "fs.inotify.max_user_instances=1024" > /etc/sysctl.d/fs-inotify-max_user_instances.conf
-  sysctl -w fs.inotify.max_user_instances=1024
-
-  echo "fs.inotify.max_user_watches=1048576" > /etc/sysctl.d/fs-inotify-max_user_watches.conf
-  sysctl -w fs.inotify.max_user_watches=1048576
-
-  echo "vm.overcommit_memory=1" >> /etc/sysctl.d/90-kubelet.conf
-  sysctl -w vm.overcommit_memory=1
-  echo "kernel.panic=10" >> /etc/sysctl.d/90-kubelet.conf
-  sysctl -w kernel.panic=10
-  echo "kernel.panic_on_oops=1" >> /etc/sysctl.d/90-kubelet.conf
-  sysctl -w kernel.panic_on_oops=1
-
-  sysctl -p
-
-  modprobe br_netfilter
-  modprobe xt_REDIRECT
-  modprobe xt_owner
-  modprobe xt_statistic
-'
+  name          = "agent"
+  vpc_id        = var.vpc_id
+  subnets       = var.private_subnets
+  ami           = var.ami
+  asg           = { min : 2, max : 2, desired : 2, termination_policies : ["Default"] }
+  spot          = false
+  instance_type = "m5.2xlarge"
+  tags = {
+    name = var.name
+  }
+  block_device_mappings = {
+    size      = 100
+    encrypted = true
+    type      = "gp3"
+  }
+  enable_ccm                = true
+  iam_permissions_boundary  = var.iam_permissions_boundary
+  download                  = false
+  rke2_config               = <<-EOF
+disable:
+  - rke2-ingress-nginx
+  - rke2-metrics-server
 EOF
+  wait_for_capacity_timeout = "20m"
+  # Required data for identifying cluster to join
+  cluster_data = module.rke2.cluster_data
 }
 
 resource "null_resource" "kubeconfig" {
