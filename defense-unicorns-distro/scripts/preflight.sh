fd5d6ce52ae14ffaad9178055a45b7a5079fb5f0
Noah Birrer <noah@defenseunicorns.com>
2023-08-21 16:52:03 -0400

    feat: adding more preflight checks

--- defense-unicorns-distro/scripts/preflight.sh
+++ defense-unicorns-distro/scripts/preflight.sh
@@ -18,6 +18,31 @@ if [[ "$server_major_version" == "$supported_major_version" ]] && (( "$server_mi
   exit 0
 else
   echo "Server version is not supported"
-  echo "Please update your cluster to a minimum Kubernetes version of ${supported_major_version}.${server_minor_version}"
+  echo "Please update your cluster to a minimum Kubernetes version of ${supported_major_version}.${supported_minor_version}"
   exit 1
 fi
+# system has a kubecontext set
+if [ $(./zarf tools kubectl config view | egrep ^current-context | awk '{print $2}') == '""' ]; then 
+  echo "No kubecontext has been set. Please ensure that a valid context is configured before proceeding."
+  exit 1
+fi
+
+# k8s cluster is healthy per kubectl get --raw='/readyz?verbose'
+if ! [ "$(./zarf tools kubectl get --raw='/readyz' 2> /dev/null )" == 'ok' ]; then
+  ./zarf tools kubectl get --raw='/readyz?verbose'
+  echo Kubernetes cluster is not healthy! Aborting...
+  exit 1
+fi
+
+# zarf init was completed (with git-server component)
+if [ $(./zarf tools kubectl get secret -n zarf zarf-state >/dev/null 2>&1 || echo false) ]; then
+  echo Zarf has not been initialized. Please run zarf init and try again.
+  exit 1
+fi
+
+if ! [ $(./zarf tools kubectl get po -n zarf zarf-gitea-0 -ojsonpath='{.status.containerStatuses[*].ready}' 2>/dev/null) ]; then
+  echo Gitea is not running. Please ensure that gitea is configured properly.
+  exit 1
+fi  
+
+# known misconfigurations
