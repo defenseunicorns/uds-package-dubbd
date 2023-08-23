2745eb4d2b1b278a2f12afd23f7f4c53146e3e3f
Noah <40781376+noahpb@users.noreply.github.com>
2023-08-23 14:24:15 -0400

    feat: add more preflight checks (#517)
    Release-As: 0.7.0

--- defense-unicorns-distro/scripts/preflight.sh
+++ defense-unicorns-distro/scripts/preflight.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Run your command and get the output
-output="###ZARF_VAR_OUTPUT###"
+# Retrieve output from test commands as zarf variables
+cluster_version="###ZARF_VAR_CLUSTER_VERSION###"
+has_kubeconfig="###ZARF_VAR_HAS_KUBECONFIG###"
+is_healthy="###ZARF_VAR_IS_HEALTHY###"
+has_init="###ZARF_VAR_HAS_INIT###"
+gitea_running="###ZARF_VAR_GITEA_RUNNING###"
 
 # Extract the Server Version Major and Minor versions using grep and cut
-server_version=$(echo "$output" | grep -o "Server Version:.*" | cut -d" " -f3)
+server_version=$(echo "$cluster_version" | grep -o "Server Version:.*" | cut -d" " -f3)
 server_major_version=$(echo "$server_version" | cut -d"." -f1)
 server_minor_version=$(echo "$server_version" | cut -d"." -f2)
 
@@ -12,12 +16,37 @@ server_minor_version=$(echo "$server_version" | cut -d"." -f2)
 supported_major_version="v1"
 supported_minor_version="26"
 
+# system has a kubecontext set
+if [ "$has_kubeconfig" == "false" ]; then
+  echo "No kubecontext has been set. Please ensure that a valid context is configured before proceeding."
+  exit 1
+fi
+
+# k8s cluster is healthy per kubectl get --raw='/readyz?verbose'
+if [ "$is_healthy" == "false" ]; then
+  ./zarf tools kubectl get --raw='/readyz?verbose'
+  echo "Kubernetes cluster is not healthy! Aborting..."
+  exit 1
+fi
+
 # Check if the Server Version is supported
 if [[ "$server_major_version" == "$supported_major_version" ]] && (( "$server_minor_version" >= "$supported_minor_version" )); then
   echo "Server version is supported"
-  exit 0
 else
   echo "Server version is not supported"
-  echo "Please update your cluster to a minimum Kubernetes version of ${supported_major_version}.${server_minor_version}"
+  echo "Please update your cluster to a minimum Kubernetes version of ${supported_major_version}.${supported_minor_version}"
   exit 1
 fi
+
+# zarf init was completed (with git-server component)
+if [ "$has_init" == "false" ]; then
+  echo "Zarf has not been initialized. Please run zarf init and try again."
+  exit 1
+fi
+
+if [ "$gitea_running" -lt 1 ]; then
+  echo "Gitea is not running. Please ensure that gitea is configured properly."
+  exit 1
+fi
+
+# known misconfigurations
