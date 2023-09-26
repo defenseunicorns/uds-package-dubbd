5dca404c716f7e84ca48bba9f765704079b7b6ae
Micah Nagel <micah.nagel@defenseunicorns.com>
2023-09-26 09:49:07 -0600

    chore: revert

--- .github/test-infra/rke2/main.tf
+++ .github/test-infra/rke2/main.tf
@@ -5,12 +5,6 @@ provider "aws" {
 terraform {
   backend "s3" {
   }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.18.0"
-    }
-  }
 }
 
 module "rke2" {
