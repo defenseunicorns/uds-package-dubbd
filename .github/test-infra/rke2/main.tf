74d13eb6e6b6b50b02388eceba8c95b89821321a
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-12-13 15:51:24 -0700

    wip: replacing yum with apt-get in pre_userdata since ami is ubuntu

--- .github/test-infra/rke2/main.tf
+++ .github/test-infra/rke2/main.tf
@@ -22,7 +22,7 @@ terraform {
 locals {
   pre_userdata = <<-EOF
     echo "Installing awscli"
-    yum install -y unzip
+    apt-get install -y unzip
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
     sudo ./aws/install
